d9d83e1b9248d05ac2553eb29739037c3d4ce24f
xxjessiexx <yasmin.khaled.aly@gmail.com>
2026-04-10 15:40:34 +0200

    Adding comments to the system calls

--- SystemCalls.java
+++ SystemCalls.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.lang.reflect.Member;
 
 public class SystemCalls {
-    public static void print(String message) {
+    public static void print(String message) {  // prints the message to the console
         System.out.println(message);
 
     }
@@ -13,7 +13,7 @@ public class SystemCalls {
         String userInput = scanner.nextLine();
         return userInput;
     }
-      public void writeFile(String fileName, String data) {
+      public void writeFile(String fileName, String data) { // writes data to a file
         try {
             FileWriter writer = new FileWriter(fileName);
             writer.write(data);
@@ -23,7 +23,7 @@ public class SystemCalls {
         }
     }
 
-    public String readFile(String fileName) {
+    public String readFile(String fileName) {  // reads data from a file and returns it as a string
         StringBuilder content = new StringBuilder();
 
         try {
@@ -41,13 +41,13 @@ public class SystemCalls {
 
         return content.toString();
     }
-    public void ReadFromMem( Memory memory, int address){
+    public void ReadFromMem( Memory memory, int address){ // reads data from a specific memory address and prints it to the console
         MemoryWord word = memory.memory[address];
         if (word != null) {
             System.out.println(word.value);
         }
     }
-    public void WriteToMem(Memory memory, int address,String label, Object value){
+    public void WriteToMem(Memory memory, int address,String label, Object value){ // writes data to a specific memory address
         MemoryWord word = new MemoryWord();
         word.value = value;
         word.label = label;
